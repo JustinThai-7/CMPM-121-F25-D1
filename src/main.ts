da15089fdeee8bbcec0cdf441660cf113e9b413c
Justin Thai <justinthai.x@gmail.com>
2025-10-30 18:00:08 +0000

    Added automatic incrementation (step 3 complete)

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,15 @@ emojiButton.innerHTML = "🍪";
 let counter: number = 0;
 const counterDisplay = document.createElement("div");
 
-emojiButton.addEventListener("click", () => {
+emojiButton.addEventListener("click", () => { // button increments
   counter += 1;
   counterDisplay.textContent = `${counter} cookies`;
 });
+
+setInterval(() => { // automatic increments
+  counter += 1;
+  counterDisplay.textContent = `${counter} cookies`;
+}, 1000);
+
 document.body.append(emojiButton);
-document.body.append(counterDisplay)
+document.body.append(counterDisplay);
