4f2144a93036516f45af10fa768eb0fa08f1d52e
Justin Thai <justinthai.x@gmail.com>
2025-10-30 19:01:17 +0000

    Added price growth, fixed growth rate display and owned counter (step 7 complete)

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,31 @@ import "./style.css";
 const emojiButton = document.createElement("button");
 emojiButton.innerHTML = "🍪";
 const counterDisplay = document.createElement("div");
+const growthDisplay = document.createElement("div");
 
 //upgrades
 const firstUpgradeButton = document.createElement("button");
-firstUpgradeButton.textContent = "Cheap Auto-Clicker ($10)";
+let price1: number = 10;
+let owned1: number = 0;
+firstUpgradeButton.textContent = `Cheap Auto-Clicker: $${
+  Math.round(price1 * 100) / 100
+} (Owned: ${owned1})`;
 firstUpgradeButton.disabled = true; // Start disabled
 document.body.append(firstUpgradeButton);
 const secondUpgradeButton = document.createElement("button");
-secondUpgradeButton.textContent = "Auto-Clicker ($100)";
+let price2: number = 100;
+let owned2: number = 0;
+secondUpgradeButton.textContent = `Auto-Clicker: $${
+  Math.round(price2 * 100) / 100
+} (Owned: ${owned2})`;
 secondUpgradeButton.disabled = true; // Start disabled
 document.body.append(secondUpgradeButton);
 const thirdUpgradeButton = document.createElement("button");
-thirdUpgradeButton.textContent = "Deluxe Auto-Clicker ($1000)";
+let price3: number = 1000;
+let owned3: number = 0;
+thirdUpgradeButton.textContent = `Cheap Auto-Clicker: $${
+  Math.round(price3 * 100) / 100
+} (Owned: ${owned3})`;
 thirdUpgradeButton.disabled = true; // Start disabled
 document.body.append(thirdUpgradeButton);
 
@@ -22,21 +35,36 @@ let growthRate: number = 0; // units per second
 
 // upgrade event listeners
 firstUpgradeButton.addEventListener("click", () => { // upgrading
-  if (counter >= 10) {
-    counter -= 10;
+  if (counter >= price1) {
+    counter -= price1;
     growthRate += 0.1;
+    owned1 += 1;
+    price1 = price1 * 1.15;
+    firstUpgradeButton.textContent = `Cheap Auto-Clicker: $${
+      Math.round(price1 * 100) / 100
+    } (Owned: ${owned1})`;
   }
 });
 secondUpgradeButton.addEventListener("click", () => { // upgrading
   if (counter >= 100) {
     counter -= 100;
     growthRate += 2;
+    owned2 += 1;
+    price2 = price2 * 1.15;
+    secondUpgradeButton.textContent = `Auto-Clicker: $${
+      Math.round(price2 * 100) / 100
+    } (Owned: ${owned2})`;
   }
 });
 thirdUpgradeButton.addEventListener("click", () => { // upgrading
   if (counter >= 1000) {
     counter -= 1000;
     growthRate += 50;
+    owned3 += 1;
+    price3 = price3 * 1.15;
+    thirdUpgradeButton.textContent = `Deluxe Auto-Clicker: $${
+      Math.round(price3 * 100) / 100
+    } (Owned: ${owned3})`;
   }
 });
 
@@ -60,6 +88,7 @@ function update(time: number) { // frame by frame increments
   thirdUpgradeButton.disabled = counter < 1000;
 
   counterDisplay.textContent = `${counter.toFixed(2)} cookies`;
+  growthDisplay.textContent = `${growthRate} cookies/sec`;
   requestAnimationFrame(update);
 }
 
@@ -67,3 +96,4 @@ requestAnimationFrame(update);
 
 document.body.append(emojiButton);
 document.body.append(counterDisplay);
+document.body.append(growthDisplay);
