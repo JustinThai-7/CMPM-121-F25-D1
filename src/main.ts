990e3686365655ff47d86ed27eacadf713169a9b
Justin Thai <justinthai.x@gmail.com>
2025-10-30 17:56:40 +0000

    Added counter (step 2 complete)

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,12 @@
 import "./style.css";
 const emojiButton = document.createElement("button");
 emojiButton.innerHTML = "🍪";
+let counter: number = 0;
+const counterDisplay = document.createElement("div");
+
+emojiButton.addEventListener("click", () => {
+  counter += 1;
+  counterDisplay.textContent = `${counter} cookies`;
+});
 document.body.append(emojiButton);
+document.body.append(counterDisplay)
