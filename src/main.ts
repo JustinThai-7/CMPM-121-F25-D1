9b07e95c399c31f730fd0846f65b0a0661065db5
Justin Thai <justinthai.x@gmail.com>
2025-10-30 18:41:22 +0000

    Added multiple upgrade options (step 6 complete)

--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,45 @@ import "./style.css";
 const emojiButton = document.createElement("button");
 emojiButton.innerHTML = "🍪";
 const counterDisplay = document.createElement("div");
-const upgradeButton = document.createElement("button");
-upgradeButton.textContent = "Buy Auto-Clicker ($10)";
-upgradeButton.disabled = true; // Start disabled
-document.body.append(upgradeButton);
+
+//upgrades
+const firstUpgradeButton = document.createElement("button");
+firstUpgradeButton.textContent = "Cheap Auto-Clicker ($10)";
+firstUpgradeButton.disabled = true; // Start disabled
+document.body.append(firstUpgradeButton);
+const secondUpgradeButton = document.createElement("button");
+secondUpgradeButton.textContent = "Auto-Clicker ($100)";
+secondUpgradeButton.disabled = true; // Start disabled
+document.body.append(secondUpgradeButton);
+const thirdUpgradeButton = document.createElement("button");
+thirdUpgradeButton.textContent = "Deluxe Auto-Clicker ($1000)";
+thirdUpgradeButton.disabled = true; // Start disabled
+document.body.append(thirdUpgradeButton);
 
 let counter: number = 0;
 let growthRate: number = 0; // units per second
 
-upgradeButton.addEventListener("click", () => { // upgrading
+// upgrade event listeners
+firstUpgradeButton.addEventListener("click", () => { // upgrading
   if (counter >= 10) {
     counter -= 10;
-    growthRate += 1; // Now passive growth goes up!
-    // Button will re-enable if they still have >=10
+    growthRate += 0.1;
   }
 });
+secondUpgradeButton.addEventListener("click", () => { // upgrading
+  if (counter >= 100) {
+    counter -= 100;
+    growthRate += 2;
+  }
+});
+thirdUpgradeButton.addEventListener("click", () => { // upgrading
+  if (counter >= 1000) {
+    counter -= 1000;
+    growthRate += 50;
+  }
+});
+
+// clicker listener
 emojiButton.addEventListener("click", () => { // button increments
   counter += 1;
   counterDisplay.textContent = `${counter} cookies`;
@@ -31,7 +55,10 @@ function update(time: number) { // frame by frame increments
 
   counter += growthRate * deltaTime;
 
-  upgradeButton.disabled = counter < 10;
+  firstUpgradeButton.disabled = counter < 10;
+  secondUpgradeButton.disabled = counter < 100;
+  thirdUpgradeButton.disabled = counter < 1000;
+
   counterDisplay.textContent = `${counter.toFixed(2)} cookies`;
   requestAnimationFrame(update);
 }
